3a1e619938f6b763b8dd0f636e815dce6f7d4210
SaraYildirim <s352530@oslomet.no>
2024-02-14 14:35:09 +0100

    Initial commit

---
+++ src/main/java/com/example/obligatorisk_oppgave_1/ObligatoriskOppgave1Application.java
@@ -0,0 +1,13 @@
+package com.example.obligatorisk_oppgave_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoriskOppgave1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligatoriskOppgave1Application.class, args);
+    }
+
+}
