7ffe84f3295e53b9dbf25f8372d4ba01d5f1eb80
SaraYildirim <s352530@oslomet.no>
2024-02-20 14:20:24 +0100

    fixed table in js and added inputvalidations in html

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -16,6 +16,12 @@ function kjopBillett(){ /*Funksjon som kalles når man trykker på 'Kjøp billet
         telefonnr : inputTelefonnr,
         epost : inputEpost
     };
+    if (kinoBillett.film === ""){
+        document.getElementById("skrivNoeFilm").innerHTML = "Må velge en film";
+    }
+    else {
+        document.getElementById("skrivNoeFilm").innerHTML = "";
+    }
 
     if (kinoBillett.antall === ""){ /*'condition statements' for å opprette inputvalideringer for inputfeltene, som utløses dersom et inputfelt er tomt*/
         document.getElementById("skrivNoeAntall").innerHTML = "Må skrive noe i antall";
@@ -57,14 +63,24 @@ function kjopBillett(){ /*Funksjon som kalles når man trykker på 'Kjøp billet
     document.getElementById("etternavn").value = "";
     document.getElementById("telefonnr").value = "";
     document.getElementById("epost").value = "";
+
+
 }
 
-function liste() { /*Setter opp listen under 'Alle billetter'*/
-    let output = "";
+function liste(){
+    let ut = "<table><tr>" +
+        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th>" +
+        "<th>Telefonnr</th><th>Epost</th>" + "</tr>";
     for (let i = 0; i < alleBilletter.length; i++){
-        output += alleBilletter[i].film+" "+alleBilletter[i].antall+" "+alleBilletter[i].fornavn+" "+alleBilletter[i].etternavn+" "+alleBilletter[i].telefonnr+" "+alleBilletter[i].epost;
+        ut += "<tr>";
+        ut += "<td>" + alleBilletter[i].film + "</td><td>" + alleBilletter[i].antall + "</td><td>"
+        + alleBilletter[i].fornavn + "</td><td>" + alleBilletter[i].etternavn + "</td><td>"
+        + alleBilletter[i].telefonnr + "</td><td>" + alleBilletter[i].epost + "</td>";
+        ut += "</tr>";
     }
-    document.getElementById("liste").innerHTML = output;
+
+    ut += "</table>"
+    document.getElementById("liste").innerHTML = ut;
 }
 
 function slettBillett() { /*Funksjon som sletter alle billettene (tømmer arrayet)*/
